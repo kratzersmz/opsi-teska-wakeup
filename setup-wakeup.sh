2d8d85f54e519ad5375b005ed2b013468664afda
root <root@backup.paedml-linux.lokal>
2020-11-06 23:47:52 +0100

    Bugfixing...

--- setup-wakeup.sh
+++ setup-wakeup.sh
@@ -3,6 +3,7 @@
 mkdir -p /opt/teska-wakeup
 cat <<EOF > /opt/teska-wakeup/wakeup.py
 #!/bin/python
+# some functions grabbed from http://svn.opsi.org/opsi4.1/misc/time-driven-maintenance-tools/wake_clients_for_setup.py
 from __future__ import print_function
 from datetime import datetime
 import os
@@ -10,6 +11,8 @@ import sys
 import pprint
 from OPSI.Logger import Logger, LOG_WARNING
 from OPSI.Util.Ping import ping
+import json
+import time
 
 logger = Logger()
 from OPSI.Backend.BackendManager import BackendManager
@@ -26,47 +29,45 @@ def getClientIDsFromGroup(backend, groupName):
 def wakeClient(backend,clientId):
   logger.info("Waking {}", clientId)
   backend.hostControlSafe_start(clientId)
+  time.sleep(0.5)
 
-def wakeGroup(backend, groupId)
+def wakeGroup(backend, groupId):
   hostsInGroup = getClientIDsFromGroup(backend,groupId)
   try:
-    hostsInGroup = hostsInGroup[0]
-  except: IndexError:
+    checker = hostsInGroup[0]
+  except IndexError:
     raise ValueError("No Hosts in Group with id '{0}'!".format(groupId))
   for hostInGroup in hostsInGroup:
-    wakeClient(backend,host)
-    sleep(0.5)  
+    print(hostInGroup)
+    wakeClient(backend,hostInGroup)
 
-# get current date
 now = datetime.now()
 # currentime in format hh-mm, e.g. 07-00, 12-30
 currenttime = now.strftime("%H-%M")
+currenttime = "22-30"
 # Return the day of the week as an integer, where Monday is 1 and Sunday is 7. For example, date(2002, 12, 4).isoweekday() == 3, a Wednesday. 
 currentweekday = now.isoweekday()
-# GROUP = backend.host_getObjects(id = clientId)[0]
-# client.getDescription()
 
-wakeups = backending.group_getObjects(id="wakeup",description="on")
+wakeupsactive = backending.group_getObjects(id="wakeup",description="on")
+
+# check if wakeup is active(True)
 isactive = True
-
-# check if wakeup is active
 try:
-  test = wakeups[0]
+  test = wakeupsactive[0]
 except IndexError:
   isactive = False
 
 if isactive:
+  wakeups = backending.group_getObjects(parentGroupId="wakeup")
   for wakeup in wakeups:
     # check it time fits to wakeup-name, e.g. 07-00
     if (currenttime == wakeup.id):
-      wakeupdays = backending.group_getObjecs(id=wakeup, type="Hostgroup")[0]
       # check if wakeups weekday is empty. If so, assume 1..5.
-      wakeupDesc = wakeupdays.getDescription()
-      if (wakeupdDesc = 0) and 1..5 in currentweekday:
-        wakeGroup(backending,wakeup)
+      if len(wakeup.description) == 0 and int(currentweekday) in range(1,5):
+        wakeGroup(backending,wakeup.id)
       # if currentweekday is in string wakeupdays in description for wakeup, e.g. wakeup description 1,3,6 (Monday, Wednesday, Saturday) 
-      elif (wakeupDesc in currentweekday):
-        wakeGroup(backending,wakeup)
+      elif str(currentweekday) in wakeup.description:
+        wakeGroup(backending,wakeup.id)
 
 EOF
 
@@ -74,4 +75,6 @@ echo "0,10,20,30,40,50 * * * * root    /usr/bin/python /opt/teska-wakeup/wakeup.
 chown root:root -R /etc/cron.d/teska-wakeup
 chmod 0644 /etc/cron.d/teska-wakeup
 systemctl restart cron
-opsi-admin -d method createGroup wakeup on on on
+opsi-admin -d method createGroup wakeup "" on
+opsi-admin -d method createGroup 07-00  ""  "" wakeup
+opsi-admin -d method createGroup 06-50  ""  "" wakeup
